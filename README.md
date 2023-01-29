e800ff0107c805e2736ee7df8fb9a7b0b654ae26
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-01-28 23:56:48 -0800

    Added some extra styles

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 Christian Limtiaco Milestone Project #1
 
 Created in HTML, CSS and Javascript
+
+Personal Note for teachers:
+- Definitely disappointed in myself for how I've done in this Javascript class. I left my second job toward lesson #6 so I could try catching up. After I heard we had two weeks to develop this page, I used the first week to try to catch all the way up to where the class ended (Only caught up to lesson 7, planning on continuing to study through our next break so I can get better!)
+- I know there is a retake for this midterm project so I will take full advantage to create a different project to my liking. 
+- Excited for the next course so I can do better on the future assignments and projects!! I've been loving to learn how to code.
+- Thank you so much for the help that was offerred. I will take full advantage of it moving forward.
+
+Sources used for ideas:
+- ChatGPT
+- Youtube
+- MDN
+- W3schools
+- Lessons from Class
--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-document.getElementById('roll-butn').addEventListener("click", function(){
-    var dice1 = Math.floor(Math.random() * 6) +1; //This is going to generate a random number from 1 to 6.. you put the +1 after so that it includes the 6th dice roll
-    var dice2 = Math.floor(Math.random() * 6) +1;
-    document.getElementById("img1").src = "/images/dice" + dice1 + ".png";
-    document.getElementById("img2").src = "/images/dice" + dice2 + ".png";
-
-    if(dice1 > dice2){
-        alert("Player 1 wins!");
-    } else if (dice1 < dice2) {
-        alert("Player 2 wins!");
-    } else {
-        alert("It's a tie!")
-    }
-});
 
 
 document.getElementById('name-butn').addEventListener("click", function() {
@@ -23,3 +9,25 @@ document.getElementById('name-butn').addEventListener("click", function() {
         player2.innerText = newName2;
 
 });
+// const score = document.querySelector();
+// let player1Score = 0;
+// let player2Score = 0;
+
+document.getElementById('roll-butn').addEventListener("click", function(){
+    var dice1 = Math.floor(Math.random() * 6) +1; //This is going to generate a random number from 1 to 6.. you put the +1 after so that it includes the 6th dice roll
+    var dice2 = Math.floor(Math.random() * 6) +1;
+    document.getElementById("img1").src = "/images/dice" + dice1 + ".png";
+    document.getElementById("img2").src = "/images/dice" + dice2 + ".png";
+
+    if(dice1 > dice2){
+        alert("Player 1 wins!");
+//        player1Score++;
+    } else if (dice1 < dice2) {
+        alert("Player 2 wins!");
+//        player2score++;
+    } else {
+        alert("It's a tie!")
+    }
+});
+
+
