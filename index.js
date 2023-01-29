6689b9c4d159aee91f0894a20d52150e251db9cc
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-01-28 23:24:34 -0800

    Got both the buttons functioning. Only problem is that the Roll the Dice button still shows the winner prior to the dice change on the img

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const rollButn = getElementById('roll-butn');
-
-function randomizeDice() {
+document.getElementById('roll-butn').addEventListener("click", function(){
     var dice1 = Math.floor(Math.random() * 6) +1; //This is going to generate a random number from 1 to 6.. you put the +1 after so that it includes the 6th dice roll
     var dice2 = Math.floor(Math.random() * 6) +1;
     document.getElementById("img1").src = "/images/dice" + dice1 + ".png";
@@ -13,18 +11,15 @@ function randomizeDice() {
     } else {
         alert("It's a tie!")
     }
-}
+});
 
-const nameChange = document.getElementById('name-butn')
-const player1 = document.getElementById('player1')
-const player2 = document.getElementById('player2')
 
-function changeNames() {
-    const newName1 = prompt("Enter name for Player 1");
-    const newName2 = prompt("Enter name for Player 2");
-    player1.innerText = newName1;
-    player2.innerText = newName2;
-}
+document.getElementById('name-butn').addEventListener("click", function() {
+    const player1 = document.getElementById('player1')
+    const player2 = document.getElementById('player2')
+        const newName1 = prompt("Enter name for Player 1");
+        const newName2 = prompt("Enter name for Player 2");
+        player1.innerText = newName1;
+        player2.innerText = newName2;
 
-rollButn.addEventListener("click", randomizeDice);
-nameChange.addEventListener("click", changeNames());
+});
