826c062cda4e7bfe3f31cf923f40c96cd63f6ec1
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-01-28 22:50:53 -0800

    Got the roll dice button to work but its displaying that the player won first, but then shows the dice face change after. So now working on fixing that

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ document.getElementById('name-butn').addEventListener("click", function() {
 
 
 // Roll the dice button
+// Math.floor rounds the number down to keep it a whole number
+var score = 0;
+
+
 document.getElementById('roll-butn').addEventListener("click", function(){
-    var dice1 = Math.floor(Math.random() * 6) +1;
+    var dice1 = Math.floor(Math.random() * 6) +1; //This is going to generate a random number from 1 to 6.. you put the +1 after so that it includes the 6th dice roll
     var dice2 = Math.floor(Math.random() * 6) +1;
-    document.getElementById("img1").src = "dice" + dice1 + ".png";
-    document.getElementById("img2").src = "dice" + dice2 + ".png";
-    console.log(function())
-    // if(dice1 > dice2){
-    //     alert("Player 1 wins!");
-    // } else if (dice1 < dice2) {
-    //     alert("Player 2 wins!");
-    // } else {
-    //     alert("It's a tie!")
-    // }
+    document.getElementById("img1").src = "/images/dice" + dice1 + ".png";
+    document.getElementById("img2").src = "/images/dice" + dice2 + ".png";
+
+    if(dice1 > dice2){
+        alert("Player 1 wins!");
+    } else if (dice1 < dice2) {
+        alert("Player 2 wins!");
+    } else {
+        alert("It's a tie!")
+    }
 });
